c228bde072682e3387d2cb36b03b2bf431e60827
Timothy Trowbridge <92933708+neotrow@users.noreply.github.com>
2023-07-25 17:34:52 +0200

    JSdoc eslint plugin (#2)

--- src/lib/enum.ts
+++ src/lib/enum.ts
@@ -1,8 +1,17 @@
+/**
+ * The standard enum type
+ */
 type StandardEnum<T> = {
   [id: string]: T | string;
   [nu: number]: string;
 };
 
+/**
+ * Returns the name of an enum from its value
+ * @param enumVariable The enum for which you want to get the name
+ * @param enumValue The value of the enum for which you want to get the name
+ * @returns A string containing the name of the enum
+ */
 function getEnumNameFromValue<T>(enumVariable: StandardEnum<T>, enumValue: T): string {
   return Object.keys(enumVariable)[Object.values(enumVariable).findIndex((x) => x === enumValue)];
 }

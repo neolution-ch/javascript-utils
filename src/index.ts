364299f87a4be69b20b89894b864d9950c579178
Daniele Debernardi <drebrez@gmail.com>
2024-01-29 13:43:15 +0100

    Add CacheProvider<TEnum> utility class (#23)
    Resolves #10 and #25

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+export * from "./lib/cacheProvider";
 export * from "./lib/date";
 export * from "./lib/enum";
+export * from "./lib/guid";
 export * from "./lib/localStorage";
 export * from "./lib/string";
-export * from "./lib/guid";

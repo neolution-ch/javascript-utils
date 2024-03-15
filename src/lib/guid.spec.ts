39c68b3725d852bfbbc01b5eb24db55eb7ef8a71
Michele Masciavè <61880943+Michele-Masciave@users.noreply.github.com>
2024-03-15 13:34:17 +0100

    Add isValidGuid function (#35)
    This PR implements a wrapper function `isValidGuid` able to validate a
    string to check whether it is guid uuid v4.
    Closes #34

--- src/lib/guid.spec.ts
+++ src/lib/guid.spec.ts
@@ -1,4 +1,4 @@
-import { newGuid, emptyGuid } from "./guid";
+import { newGuid, emptyGuid, isValidGuid } from "./guid";
 
 describe("guid tests", () => {
   test("newGuid", () => {
@@ -7,5 +7,24 @@ describe("guid tests", () => {
 
   test("emptyGuid", () => {
     expect(emptyGuid).toBe("00000000-0000-0000-0000-000000000000");
+  });
+
+  test.each([
+    [null as unknown as string, false],
+    [undefined as unknown as string, false],
+    [0 as unknown as string, false],
+    [10 as unknown as string, false],
+    [new Date() as unknown as string, false],
+    ["", false],
+    [" ", false],
+    ["507956c7-30b3-4401-9800-e5e7f8f3276X", false],
+    ["507956c7-30b3-4401-9800-e5e7f8f32761", true],
+    [newGuid(), true],
+    [emptyGuid, true],
+    ["3B467B14-CD99-4199-8E35-82B3C37182BA", true],
+    // MAX not recognized as guid > https://github.com/uuidjs/uuid/pull/714
+    ["ffffffff-ffff-ffff-ffff-ffffffffffff", false],
+  ])("isValidGuid", (value, expected) => {
+    expect(isValidGuid(value)).toBe(expected);
   });
 });

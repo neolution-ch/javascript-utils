aae287ec505ff9a777592494b193d915a71e2e21
Daniele Debernardi <drebrez@gmail.com>
2024-04-30 13:25:26 +0200

    Add more date utility functions (#33)

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,4 +1,13 @@
-import { isDate, isValid, isLastDayOfMonth, differenceInCalendarDays } from "date-fns";
+import {
+  isDate,
+  isValid,
+  isFirstDayOfMonth,
+  isLastDayOfMonth,
+  isLeapYear,
+  isWeekend,
+  differenceInCalendarDays,
+  addBusinessDays,
+} from "date-fns";
 
 /**
  * Check whether the date is valid
@@ -10,12 +19,39 @@ export function dateIsValid(date: Date): boolean {
 }
 
 /**
+ * Check whether the date is the first day of the month
+ * @param date The date
+ * @returns A boolean indicating whether the date is the first day of the month
+ */
+export function dateIsFirstDayOfMonth(date: Date): boolean {
+  return dateIsValid(date) && isFirstDayOfMonth(date);
+}
+
+/**
  * Check whether the date is the last day of the month
  * @param date The date
  * @returns A boolean indicating whether the date is the last day of the month
  */
 export function dateIsLastDayOfMonth(date: Date): boolean {
-  return isDate(date) && isLastDayOfMonth(date);
+  return dateIsValid(date) && isLastDayOfMonth(date);
+}
+
+/**
+ * Check whether the date is a leap year
+ * @param date The date
+ * @returns A boolean indicating whether the date is a leap year
+ */
+export function dateIsLeapYear(date: Date): boolean {
+  return dateIsValid(date) && isLeapYear(date);
+}
+
+/**
+ * Check whether the date is a weekend
+ * @param date The date
+ * @returns A boolean indicating whether the date is a weekend
+ */
+export function dateIsWeekend(date: Date): boolean {
+  return dateIsValid(date) && isWeekend(date);
 }
 
 /**
@@ -26,5 +62,14 @@ export function dateIsLastDayOfMonth(date: Date): boolean {
  * @returns The difference in days between two dates
  */
 export function dateDifferenceInDays(from: Date, to: Date): number {
-  return Math.abs(differenceInCalendarDays(from, to));
+  return dateIsValid(from) && dateIsValid(to) ? Math.abs(differenceInCalendarDays(from, to)) : Number.NaN;
+}
+
+/**
+ * Get the next business date
+ * @param date The date
+ * @returns The next business date
+ */
+export function getNextBusinessDate(date: Date): Date {
+  return dateIsValid(date) ? addBusinessDays(date, 1) : new Date(Number.NaN);
 }

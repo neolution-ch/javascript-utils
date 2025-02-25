79c5be5d05b081d6b9fcce0fd51e04ce651ec0e2
Tobias Merki <103021062+neotob@users.noreply.github.com>
2025-02-24 20:07:50 -1000

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,3 @@
 import neolutionEslintConfig from "@neolution-ch/eslint-config-neolution";
 
-export default [...neolutionEslintConfig.configs.flat.typescript];
+export default [...neolutionEslintConfig.configs.flat.typescript, { rules: { "unicorn/no-null": "off" } }];

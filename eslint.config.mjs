603842e603e5b3de239ce09aed1d6beda97ec998
Daniele Debernardi <daniele.debernardi@neolution.ch>
2025-04-07 18:57:21 +0200

    add require-jsdoc config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,41 @@
 import neolutionEslintConfig from "@neolution-ch/eslint-config-neolution";
 
-export default [...neolutionEslintConfig.configs.flat.typescript, { rules: { "unicorn/no-null": "off" } }];
+export default [
+  ...neolutionEslintConfig.configs.flat.typescript,
+  {
+    ignores: ["**/coverage/"],
+  },
+  {
+    ignores: ["**/*.spec.ts"],
+    rules: {
+      "jsdoc/require-jsdoc": [
+        "error",
+        {
+          publicOnly: false,
+          require: {
+            ArrowFunctionExpression: true,
+            ClassDeclaration: true,
+            ClassExpression: true,
+            FunctionDeclaration: true,
+            FunctionExpression: true,
+            MethodDefinition: true,
+          },
+          contexts: [
+            "ArrowFunctionExpression",
+            "FunctionDeclaration",
+            "FunctionExpression",
+            "MethodDefinition",
+            "PropertyDefinition",
+            "TSDeclareFunction",
+            "TSEnumDeclaration",
+            "TSInterfaceDeclaration",
+            "TSMethodSignature",
+            "TSPropertySignature",
+            "TSTypeAliasDeclaration",
+          ],
+          checkGetters: true,
+        },
+      ],
+    },
+  },
+];

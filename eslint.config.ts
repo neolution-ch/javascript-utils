38378cc68807964cc300ba10e7715ee7c49665b8
Tobias Merki <103021062+neotob@users.noreply.github.com>
2025-04-09 18:29:34 -1000

    update

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,43 +1,9 @@
 import neolutionEslintConfig from "@neolution-ch/eslint-config-neolution";
-import jestPlugin from "eslint-plugin-jest";
 
 export default [
-  ...neolutionEslintConfig.configs.flat.typescript,
-  jestPlugin.configs["flat/recommended"],
-  {
-    ignores: ["**/coverage/"],
-  },
-  {
-    ignores: ["**/*.spec.ts"],
-    rules: {
-      "jsdoc/require-jsdoc": [
-        "error",
-        {
-          publicOnly: false,
-          require: {
-            ArrowFunctionExpression: true,
-            ClassDeclaration: true,
-            ClassExpression: true,
-            FunctionDeclaration: true,
-            FunctionExpression: true,
-            MethodDefinition: true,
-          },
-          contexts: [
-            "ArrowFunctionExpression",
-            "FunctionDeclaration",
-            "FunctionExpression",
-            "MethodDefinition",
-            "PropertyDefinition",
-            "TSDeclareFunction",
-            "TSEnumDeclaration",
-            "TSInterfaceDeclaration",
-            "TSMethodSignature",
-            "TSPropertySignature",
-            "TSTypeAliasDeclaration",
-          ],
-          checkGetters: true,
-        },
-      ],
-    },
-  },
+  ...neolutionEslintConfig.configs.flat.getConfig({
+    ...neolutionEslintConfig.configs.flat.defaults.typescript,
+    jest: true,
+    jsdocRequireJsdoc: true,
+  }),
 ];
